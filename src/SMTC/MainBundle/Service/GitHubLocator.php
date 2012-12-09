8d7dc680f1a23b8645f7165802ac7284452d5a3a
Fran Moreno <franmomu@gmail.com>
2012-12-09 15:32:15 +0100

    Added link to Github in the code

---
+++ src/SMTC/MainBundle/Service/GitHubLocator.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace SMTC\MainBundle\Service;
+
+use CG\Core\ClassUtils;
+use Symfony\Component\Templating\TemplateNameParserInterface;
+use Symfony\Component\Config\FileLocatorInterface;
+
+class GithubLocator
+{
+    private $repository;
+    private $rootDir;
+    private $parser;
+    private $locator;
+    private $branch;
+
+    public function __construct(TemplateNameParserInterface $parser, FileLocatorInterface $locator, $repository, $rootDir)
+    {
+        $this->parser = $parser;
+        $this->locator = $locator;
+        $this->repository = $repository;
+        $this->rootDir = $rootDir;
+        $this->branch = "master";
+    }
+
+    public function setBranch($branch)
+    {
+        $this->branch = $branch;
+    }
+
+    public function getRepository()
+    {
+        return $this->repository;
+    }
+
+    public function getControllerLink($controller)
+    {
+        $class = get_class($controller[0]);
+        if (class_exists('CG\Core\ClassUtils')) {
+            $class = ClassUtils::getUserClass($class);
+        }
+
+        $r = new \ReflectionClass($class);
+        $m = $r->getMethod($controller[1]);
+
+        $relativeClassDir = str_replace(dirname($this->rootDir)."/", "", $r->getFilename());
+
+        $anchor = sprintf("#L%d-%d", $m->getStartline(), $m->getEndline());
+
+        return $this->getBaseFileLink() . "/" . $relativeClassDir . $anchor;
+    }
+
+    public function getTemplateLink($template)
+    {
+        $templateDir = $this->locator->locate($this->parser->parse($template->getTemplateName()));
+        $templateRelativeDir = str_replace(dirname($this->rootDir)."/", "", $templateDir);
+
+        return $this->getBaseFileLink() . "/" . $templateRelativeDir;
+    }
+
+    public function getBaseFileLink()
+    {
+        return $this->repository . "/" . $this->getBlobDir() . "/" . $this->branch;
+    }
+
+    private function getBlobDir()
+    {
+        return "blob";
+    }
+}
--- src/SMTC/MainBundle/Twig/Extension/ShowMeTheCodeExtension.php
+++ src/SMTC/MainBundle/Twig/Extension/ShowMeTheCodeExtension.php
@@ -5,15 +5,18 @@ namespace SMTC\MainBundle\Twig\Extension;
 use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Bundle\TwigBundle\Loader\FilesystemLoader;
 use CG\Core\ClassUtils;
+use SMTC\MainBundle\Service\GithubLocator;
 
 class ShowMeTheCodeExtension extends \Twig_Extension
 {
     protected $loader;
     protected $controller;
+    protected $githubLocator;
 
-    public function __construct(FilesystemLoader $loader)
+    public function __construct(FilesystemLoader $loader, GithubLocator $githubLocator)
     {
         $this->loader = $loader;
+        $this->githubLocator = $githubLocator;
     }
 
     public function setController($controller)
@@ -33,6 +36,9 @@ class ShowMeTheCodeExtension extends \Twig_Extension
 
     public function getCode($template)
     {
+        $controllerLink = $this->githubLocator->getControllerLink($this->controller);
+        $templateLink = $this->githubLocator->getTemplateLink($template);
+
         $controller = htmlspecialchars($this->getControllerCode(), ENT_QUOTES, 'UTF-8');
         $template = htmlspecialchars($this->getTemplateCode($template), ENT_QUOTES, 'UTF-8');
 
@@ -40,10 +46,10 @@ class ShowMeTheCodeExtension extends \Twig_Extension
         $template = str_replace('{% set code = code(_self) %}', '', $template);
 
         return <<<EOF
-<p><strong>Controller Code</strong></p>
+<h4><strong><a href="$controllerLink">Controller Code</a></strong></h4>
 <pre class="prettyprint">$controller</pre>
 
-<p><strong>Template Code</strong></p>
+<h4><strong><a href="$templateLink">Template Code</a></strong></h4>
 <pre class="prettyprint">$template</pre>
 EOF;
     }
