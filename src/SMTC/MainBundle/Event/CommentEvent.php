b95276d803896ab882cd6aaf99c051d05739fd3c
Fran Moreno <franmomu@gmail.com>
2013-03-30 18:24:59 +0100

    CommentIpListener no longer needs request scope

--- src/SMTC/MainBundle/Event/CommentEvent.php
+++ src/SMTC/MainBundle/Event/CommentEvent.php
@@ -4,14 +4,22 @@ namespace SMTC\MainBundle\Event;
 
 use Symfony\Component\EventDispatcher\Event;
 use SMTC\MainBundle\Model\Comment;
+use Symfony\Component\HttpFoundation\Request;
 
 class CommentEvent extends Event
 {
     private $comment;
+    private $request;
 
-    public function __construct(Comment $comment)
+    public function __construct(Request $request, Comment $comment)
     {
         $this->comment = $comment;
+        $this->request = $request;
+    }
+
+    public function getRequest()
+    {
+        return $this->request;
     }
 
     public function getComment()
--- src/SMTC/MainBundle/EventListener/CommentIpListener.php
+++ src/SMTC/MainBundle/EventListener/CommentIpListener.php
@@ -2,21 +2,14 @@
 
 namespace SMTC\MainBundle\EventListener;
 
-use Symfony\Component\HttpFoundation\Request;
 use SMTC\MainBundle\Event\CommentEvent;
 
 class CommentIpListener
 {
-    private $request;
-
-    public function __construct(Request $request)
-    {
-        $this->request = $request;
-    }
-
     public function onCommentSubmitted(CommentEvent $event)
     {
         $comment = $event->getComment();
-        $comment->ip = $this->request->getClientIp();
+        $request = $event->getRequest();
+        $comment->ip = $request->getClientIp();
     }
 }
