dd00ae26ed401cb9df9c825f754889a6c46d9b6b
Fran Moreno <franmomu@gmail.com>
2013-03-31 22:27:30 +0200

    Added last example of embedding forms

--- src/SMTC/MainBundle/Controller/Example/FormController.php
+++ src/SMTC/MainBundle/Controller/Example/FormController.php
@@ -12,6 +12,7 @@ use SMTC\MainBundle\Entity\Profile;
 use SMTC\MainBundle\Entity\Address;
 use SMTC\MainBundle\Form\Type\UserProfileType;
 use SMTC\MainBundle\Form\Type\UserAddressesType;
+use SMTC\MainBundle\Form\Type\UserType;
 
 class FormController extends Controller
 {
@@ -178,4 +179,87 @@ class FormController extends Controller
         );
     }
 
+    /**
+     * @Route("/forms/user/new", name="examples_forms_user_create")
+     * @Template("MainBundle:Example\Form:new_user.html.twig")
+     */
+    public function userNewAction(Request $request)
+    {
+        $user = new User();
+        $profile = new Profile();
+        $user->setProfile($profile);
+
+        $form = $this->createForm(new UserType(), $user);
+
+        if ($request->isMethod("POST")) {
+            $form->bind($request);
+
+            if ($form->isValid()) {
+                $em = $this->getDoctrine()->getManager();
+
+                $em->persist($user);
+                // $em->flush();
+
+                $flashBag = $this->get('session')->getFlashBag();
+                $flashBag->add('smtc_success', 'Se ha creado un usuario:');
+                $flashBag->add('smtc_success', sprintf('Username: %s', $user->getUsername()));
+                $flashBag->add('smtc_success', sprintf('Nombre: %s', $user->getProfile()->getName()));
+                if (0 !== count($user->getAddresses())) {
+                    $flashBag->add('smtc_success', 'Direcciones:');
+                    foreach ($user->getAddresses() as $address) {
+                        $flashBag->add('smtc_success', sprintf('&nbsp;&nbsp;%s (%s)', $address->getStreet(), $address->getCity()->getName()));
+                    }
+                }
+
+                // Añadir evento kernel.terminate para restaurar la bbddhgycdexcfrhy
+                return $this->redirect($this->generateUrl('examples_forms_user_create'));
+            }
+        }
+
+        return array(
+            'form' => $form->createView()
+        );
+    }
+
+    /**
+     * @Route("/forms/user/{username}/edit", name="examples_forms_user_edit")
+     * @ParamConverter("user", class="MainBundle:User")
+     * @Template("MainBundle:Example\Form:edit_user.html.twig")
+     */
+    public function userEditAction(User $user, Request $request)
+    {
+        $form = $this->createForm(new UserType(), $user);
+
+        if ($request->isMethod("POST")) {
+            $form->bind($request);
+
+            if ($form->isValid()) {
+                $em = $this->getDoctrine()->getManager();
+
+                $em->persist($user);
+                // $em->flush();
+
+                $flashBag = $this->get('session')->getFlashBag();
+                $flashBag->add('smtc_success', 'Se ha editado un usuario:');
+                $flashBag->add('smtc_success', sprintf('Username: %s', $user->getUsername()));
+                $flashBag->add('smtc_success', sprintf('Nombre: %s', $user->getProfile()->getName()));
+                if (0 !== count($user->getAddresses())) {
+                    $flashBag->add('smtc_success', 'Direcciones:');
+                    foreach ($user->getAddresses() as $address) {
+                        $flashBag->add('smtc_success', sprintf('&nbsp;&nbsp;%s (%s)', $address->getStreet(), $address->getCity()->getName()));
+                    }
+                }
+
+                // Añadir evento kernel.terminate para restaurar la bbddhgycdexcfrhy
+                return $this->redirect($this->generateUrl('examples_forms_user_edit', array(
+                    'username' => $user->getUsername()
+                )));
+            }
+        }
+
+        return array(
+            'form' => $form->createView(),
+            'user' => $user,
+        );
+    }
 }
