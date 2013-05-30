343f59129389a9eb78a785775ea4e8b7cf3f124c
Fran Moreno <franmomu@gmail.com>
2013-05-30 22:55:03 +0200

    Added HelpTypeExtension

---
+++ src/SMTC/MainBundle/Form/Extension/HelpTypeExtension.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace SMTC\MainBundle\Form\Extension;
+
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class HelpTypeExtension extends AbstractTypeExtension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars['help'] = isset($options['help']) ? $options['help'] : "";
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setOptional(array('help'));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getExtendedType()
+    {
+        return 'field';
+    }
+}
